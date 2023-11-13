56de52b440a3576562bc4c0d0e302f4a45478b50
Linh <nguyenkieulinh1308@gmail.com>
2023-11-13 13:49:38 +0700

    sochinhphuong

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 uisng namespace std;
+bool Check_SHT(int N)
+{
+    int S = 0;
+    for (int i = 1; i < N; i++)
+        if (N % i == 0)
+            S += i;  // S = S + i
 
-int main(){
-    int a;
-    cout << "Nhap a: ";
-    cin >> a;
-
-    return 1;
+    if (S == N)
+        return true; // Là số hoàn thiện
+    return false;     // Không là số hoàn thiện
 }
 int main(){
-    int n;
-    cout <<"Nhap so nguyen n: ";
-    cin >> n;
+    int a;
+    cout << "Nhap so nguyen a: ";
+    cin >> a;
+
+     int n ;
+     cout << "nhap n";
+     cin >> n;
+     if (check_SHT(n)) {
+        cout << "la so hoan hien: ";
+     } else {
+        cout << "khong la so hoan thien";
+     }
+     return 1;
 }
