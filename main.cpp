85a8ed16d4bc12910d76227f4b0d0d05cb838a66
Linh <nguyenkieulinh1308@gmail.com>
2023-11-13 14:03:06 +0700

    sohoanthien

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 uisng namespace std;
-
+bool check_SHT(int n){
+    int s = 0;
+    for (int i = 1; i < n; i++)
+     if (n% i == 0)
+        s += i ;
+    if (s == n)
+        return true ;
+    return false;
+}
 int main(){
     int a;
     cout << "Nhap a: ";
     cin >> a;
 
     return 1;
-}
-int main(){
-    int n;
-    cout <<"Nhap so nguyen n: ";
-    cin >> n;
+
+    
 }
