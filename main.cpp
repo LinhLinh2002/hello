6648153eb35707d4b3452047f08fb6d66bb60138
Linh <nguyenkieulinh1308@gmail.com>
2023-11-13 13:16:32 +0700

    nhap so

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+uisng namespace std;
+
+int main(){
+    int a;
+    cout << "Nhap a: ";
+    cin >> a;
+
+    return 1;
+}
+int main(){
+    int n;
+    cout <<"Nhap so nguyen n: ";
+    cin >> n;
+}
